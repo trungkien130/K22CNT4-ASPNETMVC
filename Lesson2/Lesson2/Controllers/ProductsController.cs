62db0dbd6180fc5257d0beabf6979d2b66aea9e2
trungkien130 <kienlee14@gmail.com>
2024-05-10 20:44:40 +0700

    new

---
+++ Lesson2/Lesson2/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            ViewBag.name = "Lê Trung Kiên - 2210900034";
+            return View();
+        }
+        public ActionResult Details(int ? id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        public string GetName()
+        {
+            return "Lê Trung Kiên - 2210900034";
+        }
+        public JsonResult ListName()
+        {
+            string[] names= { "Hùng , Dũng , Sang , Trọng" };
+            return Json(names, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
